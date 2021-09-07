8869fb0c466f5d1d3f99c75ea1402deb38c6bc5f
IrinaShushkevych <IraOlijnyk@gmail.com>
2021-09-07 19:06:54 +0300

    create js

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import menu from './menu.json'
+import menuTpl from './menutpl.hbs'
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+}
+
+const menuEl = document.querySelector('.js-menu')
+const checkEl = document.querySelector('.theme-switch__toggle')
+const body = document.querySelector('body')
+
+const menuMarkup = menu.map((el) => menuTpl(el)).join('')
+
+menuEl.innerHTML = menuMarkup
+
+let themePage = Theme[localStorage.getItem('theme')]
+console.log(themePage)
+if (themePage) {
+  body.classList.add(Theme[localStorage.getItem('theme')])
+  if (localStorage.getItem('theme') == 'LIGHT') {
+    checkEl.checked = false
+  } else {
+    checkEl.checked = true
+  }
+} else {
+  body.classList.add(Theme[0])
+  localStorage.setItem('theme', Theme[0])
+}
+
+checkEl.addEventListener('change', setTheme)
+
+function setTheme() {
+  if (body.classList.contains('light-theme')) {
+    body.classList.replace('light-theme', 'dark-theme')
+    localStorage.setItem('theme', 'DARK')
+  } else {
+    body.classList.replace('dark-theme', 'light-theme')
+    localStorage.setItem('theme', 'LIGHT')
+  }
+}
